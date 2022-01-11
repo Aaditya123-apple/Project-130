d3f362b00f3df8524dbd6b67562579020c871474
Aaditya123-apple <68804634+Aaditya123-apple@users.noreply.github.com>
2022-01-11 13:57:34 +0530

    Add files via upload

---
+++ clean.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import csv
+
+df=pd.read_csv("final.csv")
+print(df.shape)
+del df["Luminosity"]
+del df["Star_name"]
+del df["Distance"]
+del df["Mass"]
+del df["Radius"]
+print(df.shape)
